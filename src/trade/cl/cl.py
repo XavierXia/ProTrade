b18632e520f632c76fe3068cbc574c4a10b4c121
XavierXia <xiawenxing2010@163.com>
2017-09-04 19:59:17 +0800

    modify cl-Strategy

--- src/trade/cl/cl.py
+++ src/trade/cl/cl.py
@@ -9,28 +9,32 @@ import json
 from pymongo import MongoClient
 import sys, getopt
 import pandas as pd
-from pandas import Serires, DataFrame
+from pandas import Series, DataFrame
 
 '''
 运行方式:
-	python cl.py --code 600000 --ktype D
+	python cl.py --code=600000 --ktype=D
 	python cl.py -c 600000 -k D
 '''
 
-class cl(argv):
+class cl():
 	def __init__(self):
 		conn = MongoClient("localhost", 27017)
 		self.db = conn["finance"]
 
+		argv = sys.argv[1:]
+		print "argv: ",argv
+
 		self.code = ""
 		self.ktype = ""
 		try:
-			opts, args = getopt.getopt(argv,"hc:k:",["help","code","ktype"])
+			opts, args = getopt.getopt(argv,"hc:k:",["help","code=","ktype="])
 		except getopt.GetoptError:
 			print 'python cl.py --code <gpcode> --ktype <ktype>'
 			sys.exit(2)
 
 		for opt, arg in opts:
+			print "opt,arg",opt,arg
 			if opt == '-h':
 				print 'python cl.py --code <gpcode> --ktype <ktype>'
 				sys.exit()
@@ -44,10 +48,15 @@ class cl(argv):
 	def getDataFromDB(self):
 		stockColl = self.db.stock
 		doc = stockColl.find_one({"dCode":self.code,"dktype":self.ktype},{"dData":1,"_id":0})
-		jdata = json.loads(doc['dData'])
-		self.DFdata = DataFrame(jata,columns=['date','open','high','low','close'])
+		data = pd.read_json(json.dumps(doc['dData']))
+		self.DFdata = data.reindex(columns=['date','open','high','low','close','volume'])
+		print "DFdata[-20:]: ", self.DFdata[-20:]
+
+	def buildModel(self):
+		
 
 
 if __name__ == "__main__":
-	cl = cl(sys.argv[1:])
+	cl = cl()
 	cl.getDataFromDB()
+	cl.buildModel()
